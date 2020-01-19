64faaa2554fa0d77af5a1a5e0d8cbe55e84b30e8
gregorio-saporito <59971032+gregorio-saporito@users.noreply.github.com>
2020-01-19 23:35:23 +0100

    dependecies

--- R/hello.R
+++ R/hello.R
@@ -4,12 +4,18 @@ hello_g <- function() {
 }
 
 runIR <- function() {
-  
-  library(shiny)
-  library(shinydashboard)
-  library(RCurl)
-  library(tidyverse)
 
+dependencies <- c("shiny", "shinydashboard", "RCurl", "tidyverse")
+  
+for (x in dependencies) {
+  if(x %in% rownames(installed.packages()) == T) {
+    library(x, character.only = T)
+  } else {
+    install.packages(x)
+    library(x, character.only = T)
+    Sys.sleep(5)
+  }
+}
 
 # read data ---------------------------------------------------------------
   d <- getURL("https://raw.githubusercontent.com/unimi-dse/8a65fae3/master/data/term_structure.csv")
