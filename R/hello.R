57a96fb06992cb4cc7ba72fa0eb78d55b134e896
gregorio-saporito <59971032+gregorio-saporito@users.noreply.github.com>
2020-01-19 20:04:19 +0100

    putting everything on the same script

--- R/hello.R
+++ R/hello.R
@@ -4,9 +4,53 @@ hello_g <- function() {
 }
 
 runIR <- function() {
-  install.packages("shinydashboard")
+  
   library(shiny)
   library(shinydashboard)
-  runApp("R/app", launch.browser = T)
   
+# ui side -----------------------------------------------------------------
+  ui <- dashboardPage(
+    
+    dashboardHeader(),
+    dashboardSidebar(
+      
+      sidebarMenu(
+        menuItem("tab_1", tabName = "tab_1"),
+        menuItem("tab_2", tabName = "tab_2"), 
+        menuItem("tab_3", tabName = "tab_3")
+      )
+    ),
+    dashboardBody(
+      
+      # css
+      tags$head(tags$style(HTML('
+      .tab-content {
+        margin-top: 30px;
+        margin-bottom: 30px;
+        margin-right: 30px;
+        margin-left: 30px;
+        color:red;
+      }
+      '))),
+      
+      tabItems(
+        tabItem(tabName = "tab_1",
+                HTML("<h1>hello world<h1>")),
+        tabItem(tabName = "tab_2",
+                HTML("this is")),
+        tabItem(tabName = "tab_3",
+                HTML("Greg"))
+      )
+    )
+  )
+
+# server side -------------------------------------------------------------
+  server <- function(input, output) { }
+  
+  
+# run app -----------------------------------------------------------------
+  runApp( shinyApp(ui, server), launch.browser = T )
+
 }
+
+
