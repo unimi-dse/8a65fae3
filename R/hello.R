a979b6c6d66648f8ac31040ca61c1a027f35a321
gregorio-saporito <59971032+gregorio-saporito@users.noreply.github.com>
2020-01-19 22:47:06 +0100

    modularize ui

--- R/hello.R
+++ R/hello.R
@@ -16,45 +16,9 @@ runIR <- function() {
   data <- read_csv(d)
 
 # source ui ---------------------------------------------------------------
-  script <- getURL("https://raw.githubusercontent.com/unimi-dse/8a65fae3/master/modules/ui.R")
+  script <- getURL ("https://raw.githubusercontent.com/unimi-dse/8a65fae3/master/modules/ui.R",
+                    ssl.verifypeer = FALSE)
   eval(parse(text = script))
-
-  
-# ui side -----------------------------------------------------------------
-  ui <- dashboardPage(
-    
-    dashboardHeader(),
-    dashboardSidebar(
-      
-      sidebarMenu(
-        menuItem("tab_1", tabName = "tab_1"),
-        menuItem("tab_2", tabName = "tab_2"), 
-        menuItem("tab_3", tabName = "tab_3")
-      )
-    ),
-    dashboardBody(
-      
-      # css
-      tags$head(tags$style(HTML('
-      .tab-content {
-        margin-top: 30px;
-        margin-bottom: 30px;
-        margin-right: 30px;
-        margin-left: 30px;
-        color:red;
-      }
-      '))),
-      
-      tabItems(
-        tabItem(tabName = "tab_1",
-                HTML("<h1>hello world<h1>")),
-        tabItem(tabName = "tab_2",
-                HTML("this is")),
-        tabItem(tabName = "tab_3",
-                HTML("Greg"))
-      )
-    )
-  )
 
 # server side -------------------------------------------------------------
   server <- function(input, output) { }
