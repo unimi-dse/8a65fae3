b0b35cf9be773ab0219e3f10e38969572194bf99
gregorio-saporito <59971032+gregorio-saporito@users.noreply.github.com>
2020-01-21 14:17:03 +0100

    title

--- modules/ui.R
+++ modules/ui.R
@@ -8,10 +8,7 @@ library(grid)
 
 ui <- dashboardPage(
   
-  dashboardHeader(title =
-                      tags$link(rel = "icon", type = "image/gif", href = "https://upload.wikimedia.org/wikipedia/it/0/04/Logo_Universit%C3%A0_degli_Studi_di_Milano.svg"),
-                    "Time Series Project"
-                  ),
+  dashboardHeader(title = "TS Project"),
   
   dashboardSidebar(
     
