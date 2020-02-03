33d2667dc95ca406c2346ebeaed4a5ec39d34cc4
gregorio-saporito <59971032+gregorio-saporito@users.noreply.github.com>
2020-02-03 10:35:23 +0100

    Removing extdata.R

--- R/extdata.R
+++
@@ -1,9 +0,0 @@
-#' Dataset term_structure
-#'
-#' This is a dataset containing LIBOR interbank interest rates
-#'
-#' @name term_structure
-#'
-#' @section term_structure.csv:
-#'
-#' This data is used in runIR()
