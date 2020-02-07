4f92356990bbd51b6b68c83be9f1e6d4bb1fbe63
gregorio-saporito <59971032+gregorio-saporito@users.noreply.github.com>
2020-02-07 11:39:14 +0100

    added link to dataset in documentation of Shiny app

--- R/shiny.R
+++ R/shiny.R
@@ -1,12 +1,10 @@
 #' Shiny app with exploratory analysis of the term structure of interest rates
 #' 
-#' The function works with no arguments.
+#' The function works with no arguments. The function depends on the dataset \code{\link{term_structure}}
 #'
 #' @return Runs the Shiny app retreiving the data, running the analysis and showing the report with interactive visualisations.
 #' 
-#' Dataset term_structure
-#'
-#' This is a dataset containing LIBOR interbank interest rates
+#' @seealso \code{\link{term_structure}}
 #' 
 #' @export
 runIR <- function() {
